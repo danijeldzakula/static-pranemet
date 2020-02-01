7799b667e12691ce7c8ab16cd0368dec4a5adbc8
Dado19Git <danidzaki060@gmail.com>
2020-02-01 21:00:49 +0100

    Adding new section, order place, removing little and other..

--- js/main.js
+++ js/main.js
@@ -95,4 +95,27 @@ $(document).ready(function() {
         focusOnSelect: true
     });
 
+
+    $(document).on('click', 'a[href^="#"]', function(e) {
+        // target element id
+        var id = $(this).attr('href');
+
+        // target element
+        var $id = $(id);
+        if ($id.length === 0) {
+            return;
+        }
+
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault();
+
+        // top position relative to the document (offsetTop)
+        var pos = $id.offset().top - 70;
+
+        // animated top scrolling
+        $('body, html').animate({
+            scrollTop: pos
+        });
+    });    
+
 });
